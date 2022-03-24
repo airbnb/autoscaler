d88148480ebfc4841eee8f4560c928f03a058f1b
Evan Sheng <evan.sheng@airbnb.com>
2022-03-24 16:57:50 -0700

    fix gofmt

--- cluster-autoscaler/expander/factory/chain_test.go
+++ cluster-autoscaler/expander/factory/chain_test.go
@@ -17,118 +17,118 @@ limitations under the License.
 package factory
 
 import (
-  "strings"
-  "testing"
+	"strings"
+	"testing"
 
-  "k8s.io/autoscaler/cluster-autoscaler/expander"
+	"k8s.io/autoscaler/cluster-autoscaler/expander"
 
-  "github.com/stretchr/testify/assert"
-  schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
 )
 
 type substringTestFilterStrategy struct {
-  substring string
+	substring string
 }
 
 func newSubstringTestFilterStrategy(substring string) *substringTestFilterStrategy {
-  return &substringTestFilterStrategy{
-    substring: substring,
-  }
+	return &substringTestFilterStrategy{
+		substring: substring,
+	}
 }
 
 func (s *substringTestFilterStrategy) BestOptions(expansionOptions []expander.Option, nodeInfo map[string]*schedulerframework.NodeInfo) []expander.Option {
-  var ret []expander.Option
-  for _, option := range expansionOptions {
-    if strings.Contains(option.Debug, s.substring) {
-      ret = append(ret, option)
-    }
-  }
-  return ret
+	var ret []expander.Option
+	for _, option := range expansionOptions {
+		if strings.Contains(option.Debug, s.substring) {
+			ret = append(ret, option)
+		}
+	}
+	return ret
 
 }
 
 func (s *substringTestFilterStrategy) BestOption(expansionOptions []expander.Option, nodeInfo map[string]*schedulerframework.NodeInfo) *expander.Option {
-  ret := s.BestOptions(expansionOptions, nodeInfo)
-  if len(ret) == 0 {
-    return nil
-  }
-  return &ret[0]
+	ret := s.BestOptions(expansionOptions, nodeInfo)
+	if len(ret) == 0 {
+		return nil
+	}
+	return &ret[0]
 }
 
 func TestChainStrategy_BestOption(t *testing.T) {
-  for name, tc := range map[string]struct {
-    filters  []expander.Filter
-    fallback expander.Strategy
-    options  []expander.Option
-    expected *expander.Option
-  }{
-    "selects with no filters": {
-      filters:  []expander.Filter{},
-      fallback: newSubstringTestFilterStrategy("a"),
-      options: []expander.Option{
-        *newOption("b"),
-        *newOption("a"),
-      },
-      expected: newOption("a"),
-    },
-    "filters with one filter": {
-      filters: []expander.Filter{
-        newSubstringTestFilterStrategy("a"),
-      },
-      fallback: newSubstringTestFilterStrategy("b"),
-      options: []expander.Option{
-        *newOption("ab"),
-        *newOption("b"),
-      },
-      expected: newOption("ab"),
-    },
-    "filters with multiple filters": {
-      filters: []expander.Filter{
-        newSubstringTestFilterStrategy("a"),
-        newSubstringTestFilterStrategy("b"),
-      },
-      fallback: newSubstringTestFilterStrategy("x"),
-      options: []expander.Option{
-        *newOption("xab"),
-        *newOption("xa"),
-        *newOption("x"),
-      },
-      expected: newOption("xab"),
-    },
-    "selects from multiple after filters": {
-      filters: []expander.Filter{
-        newSubstringTestFilterStrategy("x"),
-      },
-      fallback: newSubstringTestFilterStrategy("a"),
-      options: []expander.Option{
-        *newOption("xc"),
-        *newOption("xaa"),
-        *newOption("xab"),
-      },
-      expected: newOption("xaa"),
-    },
-    "short circuits": {
-      filters: []expander.Filter{
-        newSubstringTestFilterStrategy("a"),
-        newSubstringTestFilterStrategy("b"),
-      },
-      fallback: newSubstringTestFilterStrategy("x"),
-      options: []expander.Option{
-        *newOption("a"),
-      },
-      expected: newOption("a"),
-    },
-  } {
-    t.Run(name, func(t *testing.T) {
-      subject := newChainStrategy(tc.filters, tc.fallback)
-      actual := subject.BestOption(tc.options, nil)
-      assert.Equal(t, tc.expected, actual)
-    })
-  }
+	for name, tc := range map[string]struct {
+		filters  []expander.Filter
+		fallback expander.Strategy
+		options  []expander.Option
+		expected *expander.Option
+	}{
+		"selects with no filters": {
+			filters:  []expander.Filter{},
+			fallback: newSubstringTestFilterStrategy("a"),
+			options: []expander.Option{
+				*newOption("b"),
+				*newOption("a"),
+			},
+			expected: newOption("a"),
+		},
+		"filters with one filter": {
+			filters: []expander.Filter{
+				newSubstringTestFilterStrategy("a"),
+			},
+			fallback: newSubstringTestFilterStrategy("b"),
+			options: []expander.Option{
+				*newOption("ab"),
+				*newOption("b"),
+			},
+			expected: newOption("ab"),
+		},
+		"filters with multiple filters": {
+			filters: []expander.Filter{
+				newSubstringTestFilterStrategy("a"),
+				newSubstringTestFilterStrategy("b"),
+			},
+			fallback: newSubstringTestFilterStrategy("x"),
+			options: []expander.Option{
+				*newOption("xab"),
+				*newOption("xa"),
+				*newOption("x"),
+			},
+			expected: newOption("xab"),
+		},
+		"selects from multiple after filters": {
+			filters: []expander.Filter{
+				newSubstringTestFilterStrategy("x"),
+			},
+			fallback: newSubstringTestFilterStrategy("a"),
+			options: []expander.Option{
+				*newOption("xc"),
+				*newOption("xaa"),
+				*newOption("xab"),
+			},
+			expected: newOption("xaa"),
+		},
+		"short circuits": {
+			filters: []expander.Filter{
+				newSubstringTestFilterStrategy("a"),
+				newSubstringTestFilterStrategy("b"),
+			},
+			fallback: newSubstringTestFilterStrategy("x"),
+			options: []expander.Option{
+				*newOption("a"),
+			},
+			expected: newOption("a"),
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			subject := newChainStrategy(tc.filters, tc.fallback)
+			actual := subject.BestOption(tc.options, nil)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
 }
 
 func newOption(debug string) *expander.Option {
-  return &expander.Option{
-    Debug: debug,
-  }
+	return &expander.Option{
+		Debug: debug,
+	}
 }
